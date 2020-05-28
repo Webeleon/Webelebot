200979be3c7b11575c66ae996f9553732a774a0e
Julien Prugne <julien.prugne@gmail.com>
2020-05-28 12:03:27 +0200

    update greeting message

--- src/discord/listeners/listeners.service.ts
+++ src/discord/listeners/listeners.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Client, GuildMember, Message, TextChannel } from 'discord.js';
+import { Client, GuildMember, TextChannel } from 'discord.js';
 
 @Injectable()
 export class ListenersService {
@@ -14,7 +14,7 @@ export class ListenersService {
   }
 
   registerGreetings(client: Client) {
-    const greetingFR = `
+    const greeting = `
 **Pour toute question DM <@!319169441382268928>**
 
 :handshake: [Partage de connaissances](https://ub.stream/webeleon/view/762d930d-0a0c-4482-a897-ba27d0642e40/share/index.html?type=view&l=en&token=0be62205-7b49-4e20-a186-4c23a365ebd3)
@@ -23,8 +23,7 @@ export class ListenersService {
 :spider_web: Développement d'applications web et mobile
 :robot: [Développement de bot discord](https://www.fiverr.com/webeleon?up_rollout=true)
 :book: Cours sur demande
-    `;
-    const greetingEN = `
+
 **For any question DM <@!319169441382268928>**
 
 :handshake: [knowledge sharing](https://ub.stream/webeleon/view/762d930d-0a0c-4482-a897-ba27d0642e40/share/index.html?type=view&l=en&token=0be62205-7b49-4e20-a186-4c23a365ebd3)
@@ -44,12 +43,11 @@ export class ListenersService {
         );
         return;
       }
-
       (channel as TextChannel).send({
-        title: `Welcome <@!${member.user.id}> to webeleon discord server`,
-        description: member.user.locale.toUpperCase().startsWith('FR')
-          ? greetingFR
-          : greetingEN,
+        content: `Welcome <@!${member.user.id}> to webeleon discord server`,
+        embed: {
+          description: greeting,
+        },
       });
     });
   }
