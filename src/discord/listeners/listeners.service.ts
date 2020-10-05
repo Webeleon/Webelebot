764c505db898a7bcd49e907d1695dea601bb00e8
julien prugne <julien@webeleon.dev>
2020-10-05 11:28:06 +0200

    update server join message

--- src/discord/listeners/listeners.service.ts
+++ src/discord/listeners/listeners.service.ts
@@ -15,23 +15,16 @@ export class ListenersService {
 
   registerGreetings(client: Client) {
     const greeting = `
-**Pour toute question DM <@!319169441382268928>**
+*For any question DM <@!319169441382268928>**
 
-:handshake: [Partage de connaissances](https://ub.stream/webeleon/view/762d930d-0a0c-4482-a897-ba27d0642e40/share/index.html?type=view&l=en&token=0be62205-7b49-4e20-a186-4c23a365ebd3)
-:helmet_with_cross: Entraide entre développeurs remote
-
-:spider_web: Développement d'applications web et mobile
-:robot: [Développement de bot discord](https://www.fiverr.com/webeleon?up_rollout=true)
-:book: Cours sur demande
-
-**For any question DM <@!319169441382268928>**
+:twitch: [Live coding](https://www.twitch.tv/webeleon)
+:github: [Webeleon open source project](https://github.com/Webeleon)
 
 :handshake: [knowledge sharing](https://ub.stream/webeleon/view/762d930d-0a0c-4482-a897-ba27d0642e40/share/index.html?type=view&l=en&token=0be62205-7b49-4e20-a186-4c23a365ebd3)
-:helmet_with_cross: Remote software developers help
 
-:spider_web: Web and mobile application development
-:robot: [Discord bot development](https://www.fiverr.com/webeleon?up_rollout=true)
-:book: Learning sessions on demand  
+:webelecoin: get you daily webelecoin using the command \`webelecoin daily\` (gift shop coming soon)
+
+Test our new game #colonie
     `;
     client.on('guildMemberAdd', (member: GuildMember) => {
       const channel = member.guild.channels.cache.find(
